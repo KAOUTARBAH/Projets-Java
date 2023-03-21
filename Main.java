15dcfa73cff1cb2eeadc4ee023796a9749d15214
Kaoutar BAHLOULI <bahloulikaoutar@gmail.com>
2023-03-21 09:24:10 +0100

    exercice 1 2

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main( String[] args ){
+        int un_int;
+        long un_long;
+        short un_short;
+        /*Type primitif	
+        byte : un entier relatif très court (entre −128 et 127). 
+        short : un entier relatif court (entre −32 768 et 32 767).
+        int : un entier relatif (entre −2 147 483 648 et 2 147 483 647). 
+        long : un entier relatif long (entre −9 223 372 036 854 776 000 et 9 223 372 036 854 776 000).
+       */
+        //Le premier bit d'une rangée de 8 bit a la valeur 0 ou 1.
+        byte un_byte;
+        //deckarer un constante 
+        //final double const = 3,14;
+        
+        int t = 20;
+        String s="KAOUTAR";
+         int x=10;
+        int y=20;
+        int res=x+y;
+        char mon_char = 41;
+        System.out.println(int(mon_char));
+        System.out.printf("The sum of %d and %d is %d.",x,y,res);
+       
+        System.out.println("Hello world\n");
+        System.out.println("\t12345\"");
+        System.out.println(25);
+        System.out.println(5+67);
+        System.out.println(t);
+        System.out.println("le programme "+s +" "+ t);
+      //  System.out.printf("The programme of %d and %d is %d.",s,t);
+
+        
+         
+
+    }
+}
