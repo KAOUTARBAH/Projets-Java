945f5d79f93208194344376a64249d273e10a1e7
Kaoutar BAHLOULI <bahloulikaoutar@gmail.com>
2023-03-21 10:17:06 +0100

    test

--- Exer2.java
+++ Exer2.java
@@ -17,30 +17,63 @@ public class Exer2 {
         double note =13.8542;
         String matiere="Anglais";
 
-        System.out.println("Le nom d'étudient est " +etudiant+ " la note : " + note + " pour la matière " + matiere);
+        System.out.println("Le nom d'étudient est " +etudiant+ " la note : " + note + "/20 pour la matière " + matiere);
 
         //Formatage avec String.format
          System.out.println(
             String.format
-             ("Le nom d'étudient est  %s et la note %f pour la matière du %s ",etudiant,note, matiere)
+             ("Le nom d'étudient est  %s et la note %f/20 pour la matière du %s ",etudiant,note, matiere)
             );
 
         //Formatage avec String.format
         //afficher que 2 chiffres aprés la virgule
          System.out.println(
             String.format
-             ("Le nom d'étudient est  %s et la note %.2f pour la matière du %s ",etudiant,note, matiere)
+             ("Le nom d'étudient est  %s et la note %.2f/20 pour la matière du %s ",etudiant,note, matiere)
             );
    
       // Afficher la longeur totale de la chaine affichée 
-      String ma_chaine = String.format("Le nom d'étudient est  %s et la note %.2f pour la matière du %s ",etudiant,note, matiere);
+      String ma_chaine = String.format("Le nom d'étudient est  %s et la note %.2f/20 pour la matière du %s ",etudiant,note, matiere);
       System.out.println("la longeur totale de la chaine affichée : "  +ma_chaine.length());
 
 
       System.out.println(
             String.format
-             ("Le nom d'étudient est  %s et la note %.2f pour la matière du %s ",etudiant,note, matiere).length()
+             ("Le nom d'étudient est  %s et la note %.2f/20 pour la matière du %s ",etudiant,note, matiere).length()
             );
 
     }
     }
+
+    /*
+    // 1
+        String nom = "Jean-bernard de la fontaine" ;
+
+        // 2
+        double note = 13.8542 ;
+
+        // 3
+        String matiere = "Philosophie" ;
+
+        // 4
+        System.out.println(
+            "L'étudiant " + nom + " a eu " + note + "/20 à son évaluation de " + matiere
+        );
+
+        // 5
+        String phrase = String.format(
+            "L'étudiant %s a eu %f/20 à son évaluation de %s",
+            nom, note, matiere
+        );
+        System.out.println( phrase );
+
+        // 6
+        phrase = String.format(
+            "L'étudiant %s a eu %.2f/20 à son évaluation de %s",
+            nom, note, matiere
+        );
+        System.out.println( phrase );
+
+        // 7
+        System.out.println( phrase.length() );
+     */
